095c176b91377d4d3643d8a6a19ffe6a93944459
tyh <756467256@qq.com>
2020-02-18 11:00:38 +0800

    {YunHan}{fixed}--{JAVA-Caption2-work8}:encapsulation CardMachine function

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -8,29 +8,23 @@ public class App {
 
     public static void main(String[] args) {
 
-        ArrayList<Card> cards = cardMachine();
-
         Scanner sc = new Scanner(System.in);
         int chooesCardNum = sc.nextInt();
 
         if (chooesCardNum > 52 || chooesCardNum < 0) {
           System.out.println("请选择0-52张牌");
         } else {
-            drawCards(cards, chooesCardNum);
+            cardMachine(chooesCardNum);
         }
     }
 
-    public static ArrayList<Card> cardMachine() {
+    public static void cardMachine (int chooesCardNum) {
         ArrayList<Card> cards = new ArrayList<>();
         for (Suit suit : Suit.values()) {
             for (Num num : Num.values()) {
                 cards.add(new Card(suit, num));
             }
         }
-        return cards;
-    }
-
-    public static void drawCards (ArrayList<Card> cards, int chooesCardNum) {
         Collections.shuffle(cards);
         for (int i = 0; i < chooesCardNum; i++) {
             System.out.print(cards.get(i).getSuit().getValue() + cards.get(i).getNum().getValue() + " ");
